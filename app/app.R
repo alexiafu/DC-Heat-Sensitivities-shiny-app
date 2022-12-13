a49270809f7b5c48cec53932a4944fbcfaff5afb
Evan Bowman <evan.bowmansa22@gmail.com>
2022-12-13 09:46:32 -0500

    modified selected variable of graphing and color of cooling center points

--- app/app.R
+++ app/app.R
@@ -132,8 +132,8 @@ ui <- fluidPage(
       tabPanel("Graphing",
                sidebarLayout(
                  sidebarPanel(
-                   varSelectInput("var1", "X variable", data = heat_bivariate, selected = "TOTALPOP"),
-                   varSelectInput("var2", "Y variable", data=heat_bivariate, selected = "HEI"),
+                   varSelectInput("var1", "X variable", data = heat_bivariate, selected = "HSI"),
+                   varSelectInput("var2", "Y variable", data=heat_bivariate, selected = "P_POVERTY"),
                    varSelectInput("var3", "Color variable (categorical)", data = heat_bivariate, selected = "majority_minority"),
                    sliderInput("bins_x", "Number of Bins for X Variable", min = 1, max = 50, value = 30),
                    sliderInput("bins_y","Number of Bins for Y Variable", min = 1, max = 50, value = 30),
@@ -490,7 +490,7 @@ server <- function(input, output) {
                          popup = ~popup_label,
                          stroke = F,
                          radius = 3, 
-                         fillColor= "#4DB6D0",
+                         fillColor= "blue",
                          fillOpacity = .8)
      }
     } else if (input$trees == TRUE) {
